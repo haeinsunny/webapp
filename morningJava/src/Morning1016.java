0ed906fba5861535a0eec9fb3d7944462ec52795
haeinsunny <sunnypass013@gmail.com>
2020-10-16 11:20:51 +0900

    추가수정

--- morningJava/src/Morning1016.java
+++ morningJava/src/Morning1016.java
@@ -7,7 +7,7 @@ public class Morning1016{
 
         for(int i=0; i<5; i++){
             for(int j=0; j<5; j++){
-                regAry[j][i] = cnt++;
+                regAry[i][j] = cnt++;
             }
         }
         for(int i=0; i<5; i++){
